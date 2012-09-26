3e7468934c54b8e3c60c58e1de379441f5976668
salman.gadit <salman.gadit@44e640b3-7cea-3488-f44c-f7e8aaeb1836>
2012-09-26 11:53:35 +0000

    Merged camera code into Scope project

--- trunk/ scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/src/com/example/scope/ScanFragment.java
+++ trunk/ scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/src/com/example/scope/ScanFragment.java
@@ -1,12 +1,13 @@
 package com.example.scope;
 
 import java.io.File;
-//import com.googlecode.tesseract.android.*;
+import com.googlecode.tesseract.android.*;
 import java.io.IOException;
 
 import com.example.scope.MainActivity;
 
 import android.app.Fragment;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -22,6 +23,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class ScanFragment extends Fragment {
@@ -34,6 +36,9 @@ public class ScanFragment extends Fragment {
 	private static final String TAG = "Scope.java";
 
 	protected static final String PHOTO_TAKEN = "photo_taken";
+	final int CAMERA_CAPTURE = 1;
+	final int PIC_CROP = 2;
+	private Uri outputFileUri;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,22 +68,35 @@ public class ScanFragment extends Fragment {
 	
 	public void startCameraActivity() {
 		File file = new File(_path);
-		Uri outputFileUri = Uri.fromFile(file);
+		outputFileUri = Uri.fromFile(file);
 
 		final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
 
-		startActivityForResult(intent, 0);
+		startActivityForResult(intent, CAMERA_CAPTURE);
 	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 		Log.i(TAG, "resultCode: " + resultCode);
-
+		Log.i(TAG, "requestCode: " + requestCode);
 		if (resultCode == -1) {
-			onPhotoTaken();
-		} else {
+			//user is returning from capturing an image using the camera
+    		if(requestCode == CAMERA_CAPTURE){
+    			//carry out the crop operation
+    			performCrop();
+    		}	
+			//user is returning from cropping the image
+			else if(requestCode == PIC_CROP){
+				//get the returned data
+				Bundle extras = data.getExtras();
+				//get the cropped bitmap
+				Bitmap thePic = extras.getParcelable("data");
+				onPhotoTaken(thePic);
+			}
+		} 
+		else {
 			Log.v(TAG, "User cancelled");
 		}
 	}
@@ -91,17 +109,49 @@ public class ScanFragment extends Fragment {
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		Log.i(TAG, "onRestoreInstanceState()");
 		if (savedInstanceState.getBoolean(MainActivity.PHOTO_TAKEN)) {
-			onPhotoTaken();
+			performCrop();
+		}
+	}
+	
+	private void performCrop()
+	{
+		try {
+			//call the standard crop action intent (the user device may not support it)
+			Intent cropIntent = new Intent("com.android.camera.action.CROP");
+			    //indicate image type and Uri
+			cropIntent.setDataAndType(outputFileUri, "image/*");
+			    //set crop properties
+			cropIntent.putExtra("crop", "true");
+			    //indicate aspect of desired crop
+			cropIntent.putExtra("aspectX", 1);
+			cropIntent.putExtra("aspectY", 1);
+			    //indicate output X and Y
+			cropIntent.putExtra("outputX", 256);
+			cropIntent.putExtra("outputY", 256);
+			cropIntent.putExtra("scale", false);
+			    //retrieve data on return
+			cropIntent.putExtra("return-data", true);
+			    //start the activity - we handle returning in onActivityResult
+			startActivityForResult(cropIntent, PIC_CROP);
+		}
+		catch(ActivityNotFoundException anfe){
+		    //display an error message
+		    String errorMessage = "Whoops - your device doesn't support the crop action!";
+		    Toast toast = Toast.makeText(getActivity().getApplicationContext(), errorMessage, Toast.LENGTH_SHORT);
+		    toast.show();
 		}
 	}
 
-	protected void onPhotoTaken() {
+	protected void onPhotoTaken(Bitmap croppedPic) {
 		_taken = true;
 
-		BitmapFactory.Options options = new BitmapFactory.Options();
-		options.inSampleSize = 4;
+		//BitmapFactory.Options options = new BitmapFactory.Options();
+		//options.inSampleSize = 4;
 
-		Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+		//Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+		
+		Bitmap bitmap = croppedPic;
+		
 		if (bitmap == null)
 			Log.v(TAG, _path);
 		try {
@@ -156,8 +206,6 @@ public class ScanFragment extends Fragment {
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
 		
-		
-/*
 		TessBaseAPI baseApi = new TessBaseAPI();
 		
 		baseApi.setDebug(true);
@@ -179,6 +227,7 @@ public class ScanFragment extends Fragment {
 			recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
 		}
 		
-		recognizedText = recognizedText.trim(); */
+		recognizedText = recognizedText.trim();
+		//Test1 H = new Test1();
 	}
 }
