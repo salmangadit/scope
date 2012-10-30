00ead3baf5f7c9aa356abf989da73e3054a74f98
Salman Gadit <salman.gadit@gmail.com>
2012-10-30 10:07:08 +0000

    Included Tessdata in the app and outside

---  scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/src/com/example/scope/Ocrmain.java
+++  scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/src/com/example/scope/Ocrmain.java
@@ -2,10 +2,14 @@ package com.example.scope;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -31,6 +35,50 @@ public class Ocrmain extends Activity {
 	public Bitmap my_image;
 
 	public void onCreate(Bundle savedInstanceState) {
+		String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+		for (String path : paths) {
+			File dir = new File(path);
+			if (!dir.exists()) {
+				if (!dir.mkdirs()) {
+					Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+					return;
+				} else {
+					Log.v(TAG, "Created directory " + path + " on sdcard");
+				}
+			}
+
+		}
+		
+		// lang.traineddata file with the app (in assets folder)
+		// You can get them at:
+		// http://code.google.com/p/tesseract-ocr/downloads/list
+		// This area needs work and optimization
+		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+			try {
+
+				AssetManager assetManager = getAssets();
+				InputStream in = assetManager.open("tessdata/eng.traineddata");
+				//GZIPInputStream gin = new GZIPInputStream(in);
+				OutputStream out = new FileOutputStream(DATA_PATH
+						+ "tessdata/eng.traineddata");
+
+				// Transfer bytes from in to out
+				byte[] buf = new byte[1024];
+				int len;
+				//while ((lenf = gin.read(buff)) > 0) {
+				while ((len = in.read(buf)) > 0) {
+					out.write(buf, 0, len);
+				}
+				in.close();
+				//gin.close();
+				out.close();
+				
+				Log.v(TAG, "Copied " + lang + " traineddata");
+			} catch (IOException e) {
+				Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+			}
+		}
 		super.onCreate(savedInstanceState);
 		Log.v(TAG, "enter...");
 		Log.v(TAG, "OCR...");
@@ -62,8 +110,9 @@ public class Ocrmain extends Activity {
 //        Log.v(TAG, "bitmap after argb:" + argb.getByteCount());
 //        
 		BitmapFactory.Options opt = new BitmapFactory.Options();
-        opt.inSampleSize = 2;
+        opt.inSampleSize = 4;
         myimage = BitmapFactory.decodeFile(filepath, opt);
+        Log.v(TAG, "Filepath:" + filepath);
         Log.v(TAG, "bitmap after comp:" + myimage.getByteCount());
 		
         //TessBase starts
