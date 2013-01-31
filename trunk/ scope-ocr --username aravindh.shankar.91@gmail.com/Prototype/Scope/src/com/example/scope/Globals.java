fd453b9604d7b66966a6a397c7578bb5065cc278
salman.gadit <salman.gadit@44e640b3-7cea-3488-f44c-f7e8aaeb1836>
2013-01-31 10:18:50 +0000

    Added missing layout

--- trunk/ scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/src/com/example/scope/Globals.java
+++ trunk/ scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/src/com/example/scope/Globals.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Application;
+import android.net.Uri;
 
 public class Globals extends Application {
 	private List<SegmentationResult> result = null;
-
+	private List<Uri> adaptiveResults = null;
+	
 	public List<SegmentationResult> getSegmentationResult() {
 		if (result == null) {
 			return new ArrayList<SegmentationResult>();
@@ -18,5 +20,17 @@ public class Globals extends Application {
 
 	public void setSegmentationResult(List<SegmentationResult> s) {
 		result = s;
+	}
+	
+	public List<Uri> getAdaptiveResult() {
+		if (adaptiveResults == null) {
+			return new ArrayList<Uri>();
+		}
+
+		return adaptiveResults;
+	}
+
+	public void setAdaptiveResult(List<Uri> s) {
+		adaptiveResults = s;
 	}
 }
