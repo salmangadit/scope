c46c49c4f43e12cf14ebfc34f81a36b69e191b82
salman.gadit <salman.gadit@44e640b3-7cea-3488-f44c-f7e8aaeb1836>
2013-03-08 20:06:28 +0000

    Updated Varun's crappy app

--- trunk/ scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/gen/com/example/scope/R.java
+++ trunk/ scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/gen/com/example/scope/R.java
@@ -26,48 +26,54 @@ public final class R {
         public static final int splash=0x7f020005;
     }
     public static final class id {
-        public static final int btn_add=0x7f080005;
-        public static final int btn_contacts=0x7f080006;
-        public static final int button1=0x7f080009;
-        public static final int button_save=0x7f080013;
-        public static final int confidence=0x7f080010;
-        public static final int coordinates=0x7f08000f;
-        public static final int crop=0x7f08000b;
-        public static final int done=0x7f08000c;
-        public static final int et_home_email=0x7f080004;
-        public static final int et_home_phone=0x7f080002;
-        public static final int et_mobile_phone=0x7f080001;
-        public static final int et_name=0x7f080000;
-        public static final int et_work_email=0x7f080003;
-        public static final int fragment_container=0x7f080007;
-        public static final int image=0x7f08000d;
-        public static final int imageView1=0x7f080014;
-        public static final int image_camera=0x7f080011;
-        public static final int image_upload=0x7f080015;
-        public static final int imgView=0x7f08000a;
-        public static final int menu_settings=0x7f080016;
-        public static final int result=0x7f08000e;
-        public static final int srListView=0x7f080008;
-        public static final int switch1=0x7f080012;
+        public static final int btn_add=0x7f080007;
+        public static final int btn_contacts=0x7f080008;
+        public static final int button1=0x7f08000b;
+        public static final int button_save=0x7f080017;
+        public static final int confidence=0x7f080014;
+        public static final int coordinates=0x7f080013;
+        public static final int crop=0x7f08000f;
+        public static final int done=0x7f080010;
+        public static final int et_home_email=0x7f080006;
+        public static final int et_home_phone=0x7f080004;
+        public static final int et_mobile_phone=0x7f080003;
+        public static final int et_name=0x7f080002;
+        public static final int et_work_email=0x7f080005;
+        public static final int fragment_container=0x7f080009;
+        public static final int image=0x7f080011;
+        public static final int imageView1=0x7f080018;
+        public static final int image_camera=0x7f080015;
+        public static final int image_upload=0x7f080019;
+        public static final int imgView=0x7f08000e;
+        public static final int menu_settings=0x7f08001a;
+        public static final int progressBar1=0x7f08000c;
+        public static final int progressSpinner=0x7f080000;
+        public static final int result=0x7f080012;
+        public static final int srListView=0x7f08000a;
+        public static final int status=0x7f080001;
+        public static final int switch1=0x7f080016;
+        public static final int txtProgress=0x7f08000d;
     }
     public static final class layout {
-        public static final int activity_contacts=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_result=0x7f030002;
-        public static final int cropping=0x7f030003;
-        public static final int cropscreen=0x7f030004;
-        public static final int custom_row_view=0x7f030005;
-        public static final int scanfragment=0x7f030006;
-        public static final int settingsfragment=0x7f030007;
-        public static final int splashscreen=0x7f030008;
-        public static final int uploadfragment=0x7f030009;
+        public static final int activity_central_progress=0x7f030000;
+        public static final int activity_contacts=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_result=0x7f030003;
+        public static final int cropping=0x7f030004;
+        public static final int cropscreen=0x7f030005;
+        public static final int custom_row_view=0x7f030006;
+        public static final int scanfragment=0x7f030007;
+        public static final int settingsfragment=0x7f030008;
+        public static final int splashscreen=0x7f030009;
+        public static final int uploadfragment=0x7f03000a;
     }
     public static final class menu {
-        public static final int activity_contacts=0x7f070000;
-        public static final int activity_edit=0x7f070001;
-        public static final int activity_main=0x7f070002;
-        public static final int activity_options=0x7f070003;
-        public static final int activity_result=0x7f070004;
+        public static final int activity_central_progress=0x7f070000;
+        public static final int activity_contacts=0x7f070001;
+        public static final int activity_edit=0x7f070002;
+        public static final int activity_main=0x7f070003;
+        public static final int activity_options=0x7f070004;
+        public static final int activity_result=0x7f070005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -88,6 +94,7 @@ public final class R {
         public static final int str_btn_add=0x7f040015;
         public static final int str_btn_contacts=0x7f040016;
         public static final int switch_preprocessor=0x7f04000c;
+        public static final int title_activity_central_progress=0x7f040017;
         public static final int title_activity_contacts=0x7f04000f;
         public static final int title_activity_edit=0x7f040005;
         public static final int title_activity_main=0x7f040003;
