c7e264584d926dd0e1ddc87cb3078e9e609c95b8
Salman Gadit <salman.gadit@gmail.com>
2013-03-25 19:04:52 +0000

    Added logging times for PreProcess.class

---  scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/src/com/example/scope/PreProcessAsync.java
+++  scope-ocr --username aravindh.shankar.91@gmail.com/Prototype/Scope/src/com/example/scope/PreProcessAsync.java
@@ -1,5 +1,6 @@
 package com.example.scope;
 
+import java.util.Date;
 import java.util.List;
 
 import android.app.ProgressDialog;
@@ -27,9 +28,15 @@ public class PreProcessAsync extends AsyncTask<Void, String, String> {
 	@Override
 	protected String doInBackground(Void... params) {
 		//progress.setMessage("Greyscaling image");
+		Date now = new Date();
+		
 		Greyscale grey = new Greyscale(preprocess.getApplicationContext(), uri);
 		Uri ppimage1 = grey.greyscale();
-
+		
+		Date later = new Date();
+		long diff = later.getTime() - now.getTime();
+		Log.v(TAG, "Greyscale time: " + diff);
+		now = new Date();
 		// progress.setMessage("Applying bilateral filter");
 		publishProgress("Applying bilateral filter");
 		
@@ -38,28 +45,52 @@ public class PreProcessAsync extends AsyncTask<Void, String, String> {
 //		Smoothing smoother = new Smoothing(preprocess.getApplicationContext(),
 //				ppimage);
 //		Uri ppimage1 = smoother.BilateralFilter();
-
+//		later = new Date();
+//		diff = later.getTime() - now.getTime();
+//		Log.v(TAG, "Bilateral time: " + diff);
+//		now = new Date();
+		
 		// progress.setMessage("Applying adaptive thresholding");
 		publishProgress("Applying adaptive thresholding");
 		Adpt initadpt = new Adpt(preprocess.getApplicationContext(), ppimage1,
 				"adpt1.bmp");
 		Uri ppimage2 = initadpt.thresh();
 		
+		later = new Date();
+		diff = later.getTime() - now.getTime();
+		Log.v(TAG, "Adapt thresh time: " + diff);
+		now = new Date();
+		
       Morphing morphing1 = new Morphing(preprocess.getApplicationContext(),ppimage2);
       Uri ppimage3 = morphing1.erode_iterate(30, 7);	
-
+      
+      	later = new Date();
+		diff = later.getTime() - now.getTime();
+		Log.v(TAG, "Morphing time: " + diff);
+		now = new Date();
+		
 		// progress.setMessage("Applying line segmentation");
 		publishProgress("Applying line segmentation");
 		SegmentLine segmenter = new SegmentLine(
 				preprocess.getApplicationContext(), ppimage3, ppimage1);
 		List<Uri> segmentedResults = segmenter.segLine();
 		
+		later = new Date();
+		diff = later.getTime() - now.getTime();
+		Log.v(TAG, "Line segmentation time: " + diff);
+		now = new Date();
+		
 		Log.v(TAG, "error: "+ segmentedResults.get(0));
 
 		Analyse analyser = new Analyse(preprocess.getApplicationContext(),
 				segmentedResults);
 		List <Uri> segmentedResults_final = analyser.adaptiveSplitter();
-
+		
+		later = new Date();
+		diff = later.getTime() - now.getTime();
+		Log.v(TAG, "Adaptive splitter time: " + diff);
+		//now = new Date();
+		
 		publishProgress("Cleaning segments");
 //		// Cleaner function
 //		for (int i = 0; i < segmentedResults.size(); i++) {
